a0acbd9fe5a9980d3a0c80acd003b7ecef04623d
RJ20Legend <2024ucp1335@mnit.ac.in>
2026-09-13 11:45:47 +0530

    Implemented Index of Coincidence

--- Assignment6/vigenere.h
+++ Assignment6/vigenere.h
@@ -5,11 +5,17 @@
 
 using namespace std;
 
-string vigenere_decrypt(const string& ciphertext, const string& key);
-string vigenere_encrypt(const string& plaintext, const string& key);
+string vigenere_encrypt(
+    const string& plaintext,
+    const string& key);
 
-bool verify(const string& originalCiphertext,
-            const string& plaintext,
-            const string& key);
+string vigenere_decrypt(
+    const string& ciphertext,
+    const string& key);
+
+bool verify(
+    const string& originalCiphertext,
+    const string& plaintext,
+    const string& key);
 
 #endif
